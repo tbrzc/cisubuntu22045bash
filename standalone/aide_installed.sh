3cc69e33b6c2ec829fc0ad5ae03d5d1133c7f366
Javier Andrés Villabón Morales <javillabonmo@gmail.com>
2024-11-29 18:16:57 -0500

    aide

--- standalone/aide_installed.sh
+++ standalone/aide_installed.sh
@@ -2,26 +2,23 @@
 
 set -euo pipefail
 
-
-
 # shellcheck disable=SC1091
 source ./lib/common_check_root.sh
-
-
+# shellcheck disable=SC1091
+source ./standalone/host-firewall/aide_audit_secure.sh
 
 CIS_CODE="6.1.1"
 
-
-
-
 setup() {
     apt install aide aide-common -y #no configuration
-    aideinit
+    aide -c /etc/aide/aide.conf --init
+    aide -c /etc/aide/aide.conf --checks
+    aide_conf
+    aide -c /etc/aide/aide.conf --update
     mv /var/lib/aide/aide.db.new /var/lib/aide/aide.db
 }
 
-
-main () {
+main() {
     check_root
     setup
 }
--- standalone/host-firewall/aide_audit_secure.sh
+++ standalone/host-firewall/aide_audit_secure.sh
@@ -3,31 +3,15 @@
 set -euo pipefail
 
 
-# shellcheck disable=SC1091
-source ./lib/common_check_root.sh
-
-
-
 CIS_CODE="4.1.4.11"
 
-
-
-setup() {
+aide_conf() {
 
     echo "/sbin/auditctl p+i+n+u+g+s+b+acl+xattrs+sha512
 /sbin/auditd p+i+n+u+g+s+b+acl+xattrs+sha512
 /sbin/ausearch p+i+n+u+g+s+b+acl+xattrs+sha512
 /sbin/aureport p+i+n+u+g+s+b+acl+xattrs+sha512
 /sbin/autrace p+i+n+u+g+s+b+acl+xattrs+sha512
-/sbin/augenrules p+i+n+u+g+s+b+acl+xattrs+sha512" >> /etc/aide/aide.conf
-
-    aide --update
-}
-
-
-main () {
-    check_root
-    setup
+/sbin/augenrules p+i+n+u+g+s+b+acl+xattrs+sha512" >>/etc/aide/aide.conf
 
 }
-main
